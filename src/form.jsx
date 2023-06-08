3e9a61283e3920cf389603f6590ff1679f14c2d0
jaimereyess <jaimereyesrdgz@gmail.com>
2023-06-08 21:26:45 +0100

    Random text

--- src/form.jsx
+++ src/form.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Counter from "./counter.jsx"
 import LetterFrequency from "./letters.jsx"
 import WordsFrequency from "./words.jsx"
+import { exampleText } from "./textExample.jsx"
 
+const randomText = Math.floor(Math.random() * 5)
 
 const Form = () => {
   const [words, setWords] = useState(0)
@@ -12,15 +14,19 @@ const Form = () => {
   const [text, setText] = useState("")
   const [lineCounter, setLineCounter] = useState(0)
 
-  function handleInputChange(e) {
-    const text = e.target.value.trim()
+  useEffect(() => {
+    setText(exampleText[randomText])
+    handleInputChange({ target: { value: exampleText[randomText] } })
+  }, [])
+
+  const handleInputChange = (e) => {
+    const text = e.target.value
     const wordCounter = text ? text.split(/\s+/g) : []
     const newWordCounter = wordCounter.length
     const charactersCounter = text.replace(/[\n\r]/g, "").length
-    const charactersWithoutSpaces = text.replace(/\s+/g, "").length;
+    const charactersWithoutSpaces = text.replace(/\s+/g, "").length
     const actualLetter = text.charAt(text.length - 1)
     const lineCounter = text.split(/\r\n|\r|\n/).length
-
 
     setText(text)
     setActualLetter(actualLetter)
@@ -40,7 +46,9 @@ const Form = () => {
               name="text"
               placeholder="Insert text..."
               onChange={handleInputChange}
-            ></textarea>
+            >
+              {exampleText[randomText]}
+            </textarea>
           </form>
         </div>
         <div className="col-sm-4">
@@ -53,7 +61,6 @@ const Form = () => {
             className="text-center"
           />
           {LetterFrequency[lastLetter]}
-
         </div>
       </div>
       <div className="row mt-4">
